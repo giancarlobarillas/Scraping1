942c413beab23225e879d0450b02e8fb909f2833
Scottpartacz <spartacz@hawk.iit.edu>
2019-04-22 18:02:38 -0500

    made the window resize based on its content

--- start.py
+++ start.py
@@ -6,6 +6,7 @@ from twitter_scrape import *
 
 fields = ('Consumer Key:','Consumer secret:','Access token:','Latitude:','Longitude:','Radius:')
 
+
 def makeform(root, fields):
    entries = []
    for field in fields:
@@ -20,57 +21,45 @@ def makeform(root, fields):
 
 def Reddit_GUI():
 
-    for widget in reddit_toolbar.winfo_children():
+    for widget in toolbar2.winfo_children():
         widget.destroy()
         
-    for widget in twitter_toolbar.winfo_children():
-        widget.destroy()
-
-    reddit_toolbar.update()
-
-    l1 = Label(reddit_toolbar, text = "Subreddit you wish to scrape: ")
+    l1 = Label(toolbar2, text = "Subreddit you wish to scrape: ")
 
     l1.pack()
 
-    e1 = Entry(reddit_toolbar)
+    e1 = Entry(toolbar2)
 
     e1.pack(side=TOP, fill=X, padx=5, pady=5)
 
-    btn1 = Button(reddit_toolbar, text = "Submit", width = 16, command = Reddit_Scrape)
+    btn1 = Button(toolbar2, text = "Submit", width = 16, command = Reddit_Scrape)
 
     btn1.pack(side = TOP, padx = 10, pady = 10)
 
-    reddit_toolbar.pack(fill = X, expand = True)
+    toolbar2.pack(fill = X, expand = True)
     
 def Twitter_GUI():
 
-    for widget in reddit_toolbar.winfo_children():
-        widget.destroy()
-        
-    for widget in twitter_toolbar.winfo_children():
+    for widget in toolbar2.winfo_children():
         widget.destroy()
 
-    twitter_toolbar.update()
+    ents = makeform(toolbar2, fields)
 
-    ents = makeform(twitter_toolbar, fields)
+    toolbar2.bind('<Return>', (lambda event, e=ents: fetch(e)))
 
-    twitter_toolbar.bind('<Return>', (lambda event, e=ents: fetch(e)))
-
-    btn1 = Button(twitter_toolbar, text = "Submit", width = 16, command = Twitter_Scrape)
+    btn1 = Button(toolbar2, text = "Submit", width = 16, command = Twitter_Scrape)
 
     btn1.pack(side = TOP, padx = 10, pady = 10)
     
-    twitter_toolbar.pack(fill = X, expand = True)
+    toolbar2.pack(fill = X, expand = True)
 
 window = Tk()
+
+toolbar2 = Frame(window)
  
 window.title("Scraping tool")
 
 toolbar = Frame(window)
-
-reddit_toolbar = Frame(window)
-
-twitter_toolbar = Frame(window)
 
 btn1 = Button(toolbar, text="Reddit", width = 16, command = Reddit_GUI)
 
