dd6c14f04a7373b3a04d1602fcb48c5aa9d9333b
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-29 16:28:38 -0500

    add api keys to scrape twitter

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -1,5 +1,36 @@
+import json
+import csv
+import tweepy
+import re
 
+def search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase):
+    #create authentication for accessing Twitter
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-def Twitter_Scrape():
+    #initialize Tweepy API
+    api = tweepy.API(auth)
     
-    print("Scrape")
+    #get the name of the spreadsheet we will write to
+    fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
+ 
+    #open the spreadsheet we will write to
+    #with open('%s.csv' % (fname), 'wb') as file:
+        #w = csv.writer(file)
+
+        #write header row to spreadsheet
+        #w.writerow(['timestamp', 'tweet_text', 'username', 'all_hashtags', 'followers_count'])
+
+        #for each tweet matching our hashtags, write relevant info to the spreadsheet
+    for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(100):
+        print([tweet.created_at, tweet.full_text.replace('\n',' ').encode('utf-8'), tweet.user.screen_name.encode('utf-8'), [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count])   
+
+consumer_key = "hrfDn07ZIftyGxqwj2ACnvpQT"
+consumer_secret = "TBeGi5SMzRnFz91OIGZXxZdIIiaacZmHdnlNrwTz82Xy20MrKx"
+access_token = "15432383-uKuMl9YxbQWKLDPiERYm7woexk0YeCB6wnoSMYHgP"
+access_token_secret = "IdORj2aKJP6ejhqlqLyWo8cnznfCgXGWbhjlGPvrTzE6r"
+    
+hashtag_phrase = input('Hashtag Phrase ')
+
+if __name__ == '__main__':
+    search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase)
